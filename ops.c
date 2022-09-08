7491b84df06657a60df70bdc9fade0ae8d2f51f8
Darren <darren2097@gmail.com>
2022-09-08 14:16:28 +0200

    added pop function

--- ops.c
+++ ops.c
@@ -96,7 +96,7 @@ void pop_func(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || stack == NULL)
 	{
-		printf("L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
